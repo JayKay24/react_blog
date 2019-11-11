13cda90377fd4874e61f102d259c89717cf75105
James Kinyua <jameskinyua590@gmail.com>
2019-11-11 23:21:23 +0300

    chore: implement the functional integration of the CreatePost component

--- src/post/CreatePost.js
+++ src/post/CreatePost.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function CreatePost({ user }) {
+export default function CreatePost({ user, posts, setPosts }) {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  function handleTitle(evt) {
+    setTitle(evt.target.value)
+  }
+
+  function handleContent(evt) {
+    setContent(evt.target.value)
+  }
+
+  function handleCreate() {
+    const newPost = { title, content, author: user }
+    setPosts([newPost, ...posts])
+  }
+
   return (
-    <form onSubmit={e => e.preventDefault()}>
+    <form onSubmit={e => { e.preventDefault(); handleCreate() }}>
       <div>Author: {user}</div>
       <div>
         <label htmlFor="create-title">Title:</label>
-        <input type="text" name="create-title" id="create-title" />
+        <input 
+          type="text" 
+          name="create-title" 
+          id="create-title" 
+          value={title}
+          onChange={handleTitle}
+        />
       </div>
-      <textarea></textarea>
+      <textarea value={content} onChange={handleContent}></textarea>
       <input type="submit" value="Create" />
     </form>
   )
